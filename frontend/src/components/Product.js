9a87ab8bc938f399353f14d7c687ec80c69e4d28
TechieJossy <techieJossy@users.noreply.github.com>
2021-01-04 01:01:18 +0100

    Feature: Added Rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
 	return (
 
-		<Card className="my-3 py-3 rounded">
+		<Card className="my-3 p-3 rounded">
 
 			<a href={`/product/${product._id}`}>
 				<Card.Img variant="bottom" src={product.image} />
@@ -16,7 +17,7 @@ const Product = ({ product }) => {
 				</a>
 
 				<Card.Text as="div">
-					<div className="my-3">{product.rating} stars from {product.numReviews} reviews</div>
+					<Rating value={product.rating} text={` ${product.numReviews} reviews`} />
 				</Card.Text>
 
 				<Card.Text as="h3">${product.price}</Card.Text>

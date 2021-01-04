daed7f9f435760994d658f6b53fe931bfeaa2025
TechieJossy <techieJossy@users.noreply.github.com>
2021-01-04 23:44:16 +0100

    Feature: Route links to pages using the React Router library

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Nav, Navbar, Container } from 'react-bootstrap'
 
 const Header = () => {
@@ -9,14 +10,25 @@ const Header = () => {
 			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
 
 				<Container>
-					<Navbar.Brand href="#home">Octo Purch</Navbar.Brand>
+
+					<LinkContainer to='/'>
+						<Navbar.Brand >Octo Purch</Navbar.Brand>
+					</LinkContainer>
+
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ml-auto">
-							<Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-							<Nav.Link href="/login"><i className="fas fa-user"></i> Sign In</Nav.Link>
+
+							<LinkContainer to='/cart'>
+								<Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link>
+							</LinkContainer>
+
 						</Nav>
 					</Navbar.Collapse>
+
 				</Container>
 
 			</Navbar>

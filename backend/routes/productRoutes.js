266381e23fef0faca063f7d3301e358a175fed88
TechieJossy <techieJossy@users.noreply.github.com>
2021-01-16 16:57:56 +0100

    Feature: Custom Error Handlers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -28,7 +28,8 @@ router.get('/:id', asyncHandler(async (req, res) => {
 
 	} else {
 
-		res.status(404).json({ message: 'Product not found' })
+		res.status(404)
+		throw new Error('Product not found')
 
 	}
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config();
 
@@ -16,6 +17,12 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+// 404 Handler
+app.use(notFound)
+
+// Error Middleware
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 

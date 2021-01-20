d364b332b7cbba781a8ee1e53c342edae049ae5c
TechieJossy <techieJossy@users.noreply.github.com>
2021-01-20 19:27:50 +0100

    Add cart reducer and actions to Redux store

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import { cartReducer } from '../reducers/cartReducers'
+
+export const addToCart = (id, quantity) => async (dispatch, getState) => {
+
+	const { data } = axios.get(`/api/products/${id}`)
+
+	dispatch({
+
+		type: CART_ADD_ITEM,
+		payload: {
+			product: data._id,
+			name: data.name,
+			image: data.image,
+			price: data.price,
+			stock: data.countInStock,
+			quantity
+		}
+
+	})
+
+	localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,21 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
 	productList: productListReducer,
-	productDetails: productDetailsReducer
+	productDetails: productDetailsReducer,
+	cart: cartReducer
 })
 
-const initialState = {}
+const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+
+	cart: { cartItems: cartItemsFromLocalStorage }
+
+}
 
 const middleware = [thunk]
 

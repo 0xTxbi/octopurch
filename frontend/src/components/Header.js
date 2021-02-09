9a3c0b791659f857eef5e4860b7d5419abb430a7
TechieJossy <techieJossy@users.noreply.github.com>
2021-02-09 07:51:38 +0100

    Feature: Display user's profile in navbar and add logout functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Nav, Navbar, Container } from 'react-bootstrap'
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
+
+	const dispatch = useDispatch()
+
+	const userLogin = useSelector(state => state.userLogin)
+	const { userInfo } = userLogin
+
+	// Logout handler
+	const logoutHandler = () => {
+
+		dispatch(logout())
+
+	}
+
 	return (
 
 		<header>
@@ -22,17 +37,27 @@ const Header = () => {
 							<LinkContainer to='/cart'>
 								<Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
 							</LinkContainer>
-							<LinkContainer to='/login'>
-								<Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link>
-							</LinkContainer>
+							{userInfo ? (
+								<NavDropdown title={userInfo.name} id="username">
+									<LinkContainer to='/profile'>
+										<NavDropdown.Item>Profile</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+								</NavDropdown>
+							) : (
+								<LinkContainer to='/login'>
+									<Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link>
+								</LinkContainer>
+
+							)}
 
 						</Nav>
 					</Navbar.Collapse>
 
 				</Container>
 
-			</Navbar>
-		</header>
+			</Navbar >
+		</header >
 
 	)
 }

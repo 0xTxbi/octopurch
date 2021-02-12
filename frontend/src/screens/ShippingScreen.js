058e4157af35872d2c26406157887f2002011618
TechieJossy <techieJossy@users.noreply.github.com>
2021-02-12 17:03:05 +0100

    Feature: Create Checkout Steps component and implement on Shipping Screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingScreen = ({ history }) => {
@@ -30,6 +31,9 @@ const ShippingScreen = ({ history }) => {
 
 	return (
 		<FormContainer>
+
+			<CheckoutSteps step1 step2 />
+
 			<h1>Shipping</h1>
 
 			<Form onSubmit={submitHandler}>
@@ -61,7 +65,7 @@ const ShippingScreen = ({ history }) => {
 				<Button type="submit" variant="primary">Continue</Button>
 
 			</Form>
-		</FormContainer>
+		</FormContainer >
 	)
 
 }
